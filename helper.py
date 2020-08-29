1ae519af1e247eba81be30ad490f781ac5510ac9
erikjoy <erikjoy@users.noreply.github.com>
2020-08-28 22:24:41 -0400

    Improved helper.py

--- helper.py
+++ helper.py
@@ -3,18 +3,16 @@ from appraiser import FundAppraiser, PortfolioAppraiser
 
 
 class ProcessConfig:
-    def __init__(self, config):
-        self.config = config
-
-    def _get_holdings_data(self):
+    @staticmethod
+    def _get_holdings_data(config):
         holdings = {}
         cash_equivalents_percentage, total_percentage = 0, 0
-        with open(self.config['holdings_file_path'], 'r') as f:
+        with open(config['holdings_file_path'], 'r') as f:
             for line in f:
                 (symbol, percentage) = line.split()
                 percentage = float(percentage)
                 total_percentage += percentage
-                if symbol == self.config['cash_equivalents_name']:
+                if symbol == config['cash_equivalents_name']:
                     cash_equivalents_percentage += percentage
                 else:
                     holdings[symbol] = percentage
@@ -22,30 +20,29 @@ class ProcessConfig:
             raise HoldingsPercentageError('The percentages of the holdings don\'t sum up to 100')
         return (holdings, cash_equivalents_percentage)
 
-    def _select_appraiser(self):
-        (holdings, cash_equivalents_percentage) = self._get_holdings_data()
-        if self.config['is_fund']:
+    @staticmethod
+    def _select_appraiser(config):
+        (holdings, cash_equivalents_percentage) = ProcessConfig._get_holdings_data(config)
+        if config['is_fund']:
             return FundAppraiser(
                 holdings,
                 cash_equivalents_percentage,
-                self.config['cash_equivalents_growth'],
-                self.config['fund_symbol'])
+                config['cash_equivalents_growth'],
+                config['fund_symbol'])
         else:
             return PortfolioAppraiser(
                 holdings,
                 cash_equivalents_percentage,
-                self.config['cash_equivalents_growth'])
+                config['cash_equivalents_growth'])
 
     @staticmethod
     def process(config):
-        settings = ProcessConfig(config)
-        return settings._select_appraiser()
+        return ProcessConfig._select_appraiser(config)
 
     @staticmethod
     def process_path(config_file_path):
         config = load(open(config_file_path, 'r'))
-        settings = ProcessConfig(config)
-        return settings._select_appraiser()
+        return ProcessConfig.process(config)
 
 
 class HoldingsPercentageError(Exception):
